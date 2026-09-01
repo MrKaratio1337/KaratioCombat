505cf2ad2a83c9a7d2004ff9a52ea237b107b6e3
MrKaratio1337 <pokedamiq@spoko.pl>
2026-09-01 13:01:21 +0200

    Added config creation

--- src/main/java/pl/karatiodev/combat/CombatPlugin.java
+++ src/main/java/pl/karatiodev/combat/CombatPlugin.java
@@ -3,15 +3,19 @@ package pl.karatiodev.combat;
 import dev.rollczi.litecommands.LiteCommands;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.karatiodev.combat.config.ConfigFactory;
+import pl.karatiodev.combat.config.PluginConfig;
 
 @Getter
 public class CombatPlugin extends JavaPlugin {
 
     private LiteCommands<?> liteCommands;
 
+    private PluginConfig pluginConfig;
+
     @Override
     public void onEnable() {
-
+        this.pluginConfig = ConfigFactory.load(this);
     }
 
     @Override
--- src/main/java/pl/karatiodev/combat/config/ConfigFactory.java
+++ src/main/java/pl/karatiodev/combat/config/ConfigFactory.java
@@ -1,13 +1,19 @@
 package pl.karatiodev.combat.config;
 
 import eu.okaeri.configs.ConfigManager;
+import eu.okaeri.configs.yaml.snakeyaml.YamlSnakeYamlConfigurer;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class ConfigFactory {
 
     public static PluginConfig load(JavaPlugin plugin){
         return ConfigManager.create(PluginConfig.class, config -> {
-
+            config.withConfigurer(new YamlSnakeYamlConfigurer());
+            config.withBindFile(new File(plugin.getDataFolder(), "config.yml"));
+            config.saveDefaults();
+            config.load(true);
         });
     }
 }
